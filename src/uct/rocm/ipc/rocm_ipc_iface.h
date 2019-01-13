c100a9a06f720d8c66d39b3516411300fc49f5b6
Qiang Yu <Qiang.Yu@amd.com>
2019-01-13 20:13:10 +0800

    UCT/ROCM: add ipc backend

---
+++ src/uct/rocm/ipc/rocm_ipc_iface.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) Advanced Micro Devices, Inc. 2019. ALL RIGHTS RESERVED.
+ * See file LICENSE for terms.
+ */
+
+
+#ifndef ROCM_IPC_IFACE_H
+#define ROCM_IPC_IFACE_H
+
+#include <uct/base/uct_iface.h>
+
+#define UCT_ROCM_IPC_TL_NAME "rocm_ipc"
+
+typedef struct uct_rocm_ipc_iface {
+    uct_base_iface_t super;
+} uct_rocm_ipc_iface_t;
+
+typedef struct uct_rocm_ipc_iface_config {
+    uct_iface_config_t super;
+} uct_rocm_ipc_iface_config_t;
+
+#endif
