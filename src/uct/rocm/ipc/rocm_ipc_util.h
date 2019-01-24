ca6078562ba19ecba720974ac0b5a5069698e2d2
Qiang Yu <Qiang.Yu@amd.com>
2019-01-24 21:36:49 +0800

    UCT/ROCM/IPC: fix D2D offset buffer copy

--- src/uct/rocm/ipc/rocm_ipc_util.h
+++ src/uct/rocm/ipc/rocm_ipc_util.h
@@ -17,6 +17,6 @@ int uct_rocm_ipc_get_gpu_agents(hsa_agent_t **agents);
 hsa_status_t uct_rocm_ipc_pack_key(void *address, size_t length,
                                    uct_rocm_ipc_key_t *key);
 hsa_status_t uct_rocm_ipc_lock_ptr(void *ptr, size_t size, void **lock_ptr,
-                                   hsa_agent_t *agent);
+                                   void **base_ptr, hsa_agent_t *agent);
 
 #endif
