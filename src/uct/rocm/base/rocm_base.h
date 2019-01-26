d05e6e00f078cfb5fde4522266e7e153171073cd
Qiang Yu <Qiang.Yu@amd.com>
2019-01-26 20:55:51 +0800

    UCT/ROCM: separate util into base dir

---
+++ src/uct/rocm/base/rocm_base.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) Advanced Micro Devices, Inc. 2019. ALL RIGHTS RESERVED.
+ * See file LICENSE for terms.
+ */
+
+
+#ifndef ROCM_BASE_H
+#define ROCM_BASE_H
+
+#include <hsa.h>
+
+hsa_status_t uct_rocm_base_init(void);
+hsa_agent_t uct_rocm_base_get_dev_agent(int dev_num);
+int uct_rocm_base_is_gpu_agent(hsa_agent_t agent);
+int uct_rocm_base_get_gpu_agents(hsa_agent_t **agents);
+int uct_rocm_base_get_dev_num(hsa_agent_t agent);
+hsa_status_t uct_rocm_base_lock_ptr(void *ptr, size_t size, void **lock_ptr,
+                                    void **base_ptr, hsa_agent_t *agent);
+
+#endif
