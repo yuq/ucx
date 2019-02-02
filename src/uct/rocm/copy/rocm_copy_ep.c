89cfdefabeedde919fa23505106c4725b957074a
Qiang Yu <Qiang.Yu@amd.com>
2019-02-02 15:34:27 +0800

    Revert "UCT/ROCM/COPY: use hsa_memory_copy"
    This reverts commit e0f8b4d71fbfe6f5ae6c1840cf7eab23a41f4871.

--- src/uct/rocm/copy/rocm_copy_ep.c
+++ src/uct/rocm/copy/rocm_copy_ep.c
@@ -10,8 +10,6 @@
 #include <ucs/debug/memtrack.h>
 #include <ucs/type/class.h>
 
-#include <hsa.h>
-
 static UCS_CLASS_INIT_FUNC(uct_rocm_copy_ep_t, uct_iface_t *tl_iface,
                            const uct_device_addr_t *dev_addr,
                            const uct_iface_addr_t *iface_addr)
@@ -92,8 +90,7 @@ ucs_status_t uct_rocm_copy_ep_put_short(uct_ep_h tl_ep, const void *buffer,
                                         unsigned length, uint64_t remote_addr,
                                         uct_rkey_t rkey)
 {
-    //memcpy((void *)remote_addr, buffer, length);
-    assert(hsa_memory_copy((void *)remote_addr, buffer, length) == HSA_STATUS_SUCCESS);
+    memcpy((void *)remote_addr, buffer, length);
 
     UCT_TL_EP_STAT_OP(ucs_derived_of(tl_ep, uct_base_ep_t), PUT, SHORT, length);
     ucs_trace_data("PUT_SHORT size %d from %p to %p",
@@ -106,8 +103,7 @@ ucs_status_t uct_rocm_copy_ep_get_short(uct_ep_h tl_ep, void *buffer,
                                         uct_rkey_t rkey)
 {
     /* device to host */
-    //memcpy(buffer, (void *)remote_addr, length);
-    assert(hsa_memory_copy(buffer, (void *)remote_addr, length) == HSA_STATUS_SUCCESS);
+    memcpy(buffer, (void *)remote_addr, length);
 
     UCT_TL_EP_STAT_OP(ucs_derived_of(tl_ep, uct_base_ep_t), GET, SHORT, length);
     ucs_trace_data("GET_SHORT size %d from %p to %p",
