ab0e3862eee6383070000aa423ee1df30d24baa2
Qiang Yu <Qiang.Yu@amd.com>
2019-02-22 14:00:36 +0800

    Revert "UCT/ROCM/COPY: use gdr copy to improve small size message performance"
    This reverts commit 956fea7b46e2b376dbe352c84c2a5b41933c8753.

--- src/uct/rocm/copy/rocm_copy_ep.h
+++ src/uct/rocm/copy/rocm_copy_ep.h
@@ -24,6 +24,16 @@ UCS_CLASS_DECLARE_NEW_FUNC(uct_rocm_copy_ep_t, uct_ep_t, uct_iface_t*,
                            const uct_device_addr_t *, const uct_iface_addr_t *);
 UCS_CLASS_DECLARE_DELETE_FUNC(uct_rocm_copy_ep_t, uct_ep_t);
 
+ucs_status_t uct_rocm_copy_ep_get_zcopy(uct_ep_h tl_ep,
+                                        const uct_iov_t *iov, size_t iovcnt,
+                                        uint64_t remote_addr, uct_rkey_t rkey,
+                                        uct_completion_t *comp);
+
+ucs_status_t uct_rocm_copy_ep_put_zcopy(uct_ep_h tl_ep,
+                                        const uct_iov_t *iov, size_t iovcnt,
+                                        uint64_t remote_addr, uct_rkey_t rkey,
+                                        uct_completion_t *comp);
+
 ucs_status_t uct_rocm_copy_ep_put_short(uct_ep_h tl_ep, const void *buffer,
                                         unsigned length, uint64_t remote_addr,
                                         uct_rkey_t rkey);
