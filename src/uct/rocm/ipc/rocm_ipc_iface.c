f2ea28fafdb865cbbe6bb783d433b162de73d6dd
Qiang Yu <Qiang.Yu@amd.com>
2019-03-13 16:11:04 +0800

    UCT/ROCM/IPC: adjust md/iface parameter
    Signed-off-by: Qiang Yu <Qiang.Yu@amd.com>

--- src/uct/rocm/ipc/rocm_ipc_iface.c
+++ src/uct/rocm/ipc/rocm_ipc_iface.c
@@ -78,11 +78,11 @@ static ucs_status_t uct_rocm_ipc_iface_query(uct_iface_h tl_iface,
                                           UCT_IFACE_FLAG_PENDING   |
                                           UCT_IFACE_FLAG_CONNECT_TO_IFACE;
 
-    /* TODO: get accurate info */
-    iface_attr->latency.overhead        = 80e-9; /* 80 ns */
+    iface_attr->latency.overhead        = 1e-9; /* ns */
     iface_attr->latency.growth          = 0;
-    iface_attr->bandwidth               = 10240 * 1024.0 * 1024.0; /* 10240 MB*/
-    iface_attr->overhead                = 0.4e-6; /* 0.4 us */
+    iface_attr->bandwidth               = 6911 * 1024.0 * 1024.0; /* Byte */
+    iface_attr->overhead                = 0; /* us */
+    iface_attr->priority                = 0;
 
     return UCS_OK;
 }
